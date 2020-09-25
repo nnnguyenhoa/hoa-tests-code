d4bca4f184d7c99abbf14f689b1f547ceb47a377
Hoa Nguyen <Hoa Nguyen>
2020-09-24 22:19:29 -0700

    minor stuff

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def my_route_page_function():
 	return render_template('home.html')
