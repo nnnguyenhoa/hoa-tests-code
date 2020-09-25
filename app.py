223ed1f4f271d226a0c1a2d971854583a2494e68
Hoa Nguyen <Hoa Nguyen>
2020-09-24 22:15:51 -0700

    minor stuff

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def my_route_page_function():
-	os.system('cmd /c "ls"')
 	return render_template('home.html')
